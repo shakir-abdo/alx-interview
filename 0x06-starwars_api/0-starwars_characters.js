992693fc8654abae401fffb3317cbf6c345aefa6
Shakir Abdo <shicolare1@gmail.com>
2024-07-11 20:04:21 +0300

    fix task 0

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,21 +1,21 @@
 #!/usr/bin/node
 
-const request = require('request')
-const movieId = process.argv[2]
-const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`
+const request = require('request');
+const movieId = process.argv[2];
+const url = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
 
 request(url, async (err, response, body) => {
   if (err) {
-    console.log(err)
+      console.log(err);
   }
   for (const characterId of JSON.parse(body).characters) {
     await new Promise((resolve, reject) => {
       request(characterId, (err, response, body) => {
         if (err) {
-          reject(err)
+            reject(err);
         }
-        console.log(JSON.parse(body).name)
-        resolve()
+          console.log(JSON.parse(body).name);
+          resolve();
       })
     })
   }
